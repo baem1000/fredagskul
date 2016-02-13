1b93c34bd249ac814cc20c58c45ea8c7ee7a78f4
gitresp <mr_larsson_@hotmail.com>
2016-02-13 14:02:36 +0100

    Update wp core + WCK
    Big update

--- wp-content/themes/fredagskul.se/single.php
+++ wp-content/themes/fredagskul.se/single.php
@@ -11,26 +11,31 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
 
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
 
-					// Previous/next post navigation.
-					twentyfourteen_post_nav();
+<?php $roligheter = get_post_meta( $post->ID, 'rolighet', true ); 
+		echo '<div class="container">';
+		foreach($roligheter as $rolighet){
+			echo '<div class="rolighet">';
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
+			$image = $rolighet['bild'];
+			$attachment_image = wp_get_attachment_image_src( $image, 'full' );
+			$src = $attachment_image[0];
+
+			if( !empty( $src ) )
+
+			echo '<a href="' .$rolighet['link']. '" target="_blank">'; // snygga till och lägg in i echo nedan
+			echo '<img src="'.$src.'" style="float:left; width:400px;"/></a>';
+
+			echo '<h3>' .$rolighet['titel']. '</h3>';
+			echo '<p>' .$rolighet['beskrivning']. '</p>';
+
+			echo '</div>';
+			echo '<div style="clear:both;"></div>';
+		}
+		echo '</div>';
+?>
+
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
@@ -38,3 +43,5 @@ get_header(); ?>
 get_sidebar( 'content' );
 get_sidebar();
 get_footer();
+
+
