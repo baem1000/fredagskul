87ea21346ef70378945f088316115b836ed99e33
Emil Bankelius <emil.bankelius@gmail.com>
2016-03-12 16:15:13 +0100

    plugins update

--- wp-content/themes/fredagskul.se/functions.php
+++ wp-content/themes/fredagskul.se/functions.php
@@ -517,3 +517,12 @@ require get_template_directory() . '/inc/customizer.php';
 if ( ! class_exists( 'Featured_Content' ) && 'plugins.php' !== $GLOBALS['pagenow'] ) {
 	require get_template_directory() . '/inc/featured-content.php';
 }
+
+function modify_contact_methods($profile_fields) {
+
+	// Add new fields
+	$profile_fields['twitter'] = 'Twitter Username';
+
+	return $profile_fields;
+}
+add_filter('user_contactmethods', 'modify_contact_methods');
